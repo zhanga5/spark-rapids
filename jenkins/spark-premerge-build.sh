ef8cbb2e40e7090d8aafaa9c8aea4ecb051bbf64
Alex Zhang <alex4zhang@gmail.com>
2021-08-26 12:05:46 +0800

    Refine stage to premerge ci 2 from integration testing per Peixin's comments
    Signed-off-by: Alex Zhang <alex4zhang@gmail.com>

--- jenkins/spark-premerge-build.sh
+++ jenkins/spark-premerge-build.sh
@@ -36,7 +36,7 @@ mvn_verify() {
     pre-commit run check-added-large-files --from-ref $BASE_REF --to-ref HEAD
 
     # Here run Python integration tests tagged with 'slow_test' only, that would require long duration or big memory. Such split would help
-    # balance test duration and memory consumption from two k8s pods running in parallel, which executes 'mvn_verify()' and 'integration_test()' respectively.
+    # balance test duration and memory consumption from two k8s pods running in parallel, which executes 'mvn_verify()' and 'ci_2()' respectively.
     mvn -U -B $MVN_URM_MIRROR '-P!snapshot-shims,pre-merge' clean verify -Dpytest.TEST_TAGS="slow_test" \
         -Dpytest.TEST_TYPE="pre-commit" -Dpytest.TEST_PARALLEL=5 -Dcuda.version=$CUDA_CLASSIFIER
 
@@ -92,8 +92,8 @@ rapids_shuffle_smoke_test() {
     $SPARK_HOME/sbin/stop-master.sh
 }
 
-integration_test() {
-    echo "Run integration testings..."
+ci_2() {
+    echo "Run premerge ci 2 testings..."
     mvn -U -B $MVN_URM_MIRROR clean package -DskipTests=true -Dcuda.version=$CUDA_CLASSIFIER
     TEST_TAGS="not slow_test" TEST_TYPE="pre-commit" TEST_PARALLEL=4 ./integration_tests/run_pyspark_from_build.sh
 }
@@ -131,15 +131,15 @@ case $BUILD_TYPE in
     all)
         echo "Run all testings..."
         mvn_verify
-        integration_test
+        ci_2
         ;;
 
     mvn_verify)
         mvn_verify
         ;;
 
-    it | integration_test)
-        integration_test
+    ci_2 )
+        ci_2
         ;;
 
     *)
